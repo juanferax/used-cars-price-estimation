0391bdf109d16db9c15e3cb2aaa559a89bb7b0ca
Juanferas <juanferas25@hotmail.com>
2021-11-18 19:45:44 -0500

    backend (Flask) and frontend (Vue) correctly configured

---
+++ backend/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__,
+            static_folder='../frontend/dist/static',
+            template_folder='../frontend/dist')
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+ 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def render_vue(path):
+    return render_template('index.html')
+
+#@app.route('/about')
+#def about():
+#    return 'This is the about endpoint'
+
+@app.route('/api/v1.0/message')
+def message():
+    return jsonify('Nuevo mensaje desde el backend')
+
+# In order to run the backend server write the following command:
+# python server.py
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+    <div class="body">
+        <h1> {{ message }} </h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Main',
+    data() {
+        return {
+            message: 'Home Page'
+        }
+    },
+    methods: {
+        getMessage() {
+            const path = 'http://localhost:5000/api/v1.0/message'
+            axios.get(path).then((response)  => {
+                this.message = response.data
+            })
+            .catch((error) => {
+                 console.error(error)
+            })
+        }
+    },
+    created() {
+        this.getMessage()
+    }
+}
+</script>
