48e1ec3bf6a03ae12b3a8ed30f4665d30340ec5a
Danna García <1193251859@u.icesi.edu.co>
2021-11-21 13:38:24 -0500

    Changed frontend

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,16 +1,15 @@
 <template lang="html">
-    <div class="container relative w-full bg-gradient-to-r from-green-400 to-blue-500">
+    <div class=" w-full bg-backgroundColor">
         <main class="min-h-screen">
             <!-- Header -->
             <div id="header" class="mx-5 pt-5">
-                <div class="pt-5 flex flex-col justify-center rounded-xl border border-black bg-gradient-to-br from-black via-blue-800 to-black">
-                    <label id ="main-tittle" class="text-center text-white font-semibold my-2 mb-8 text-7xl text-">
-                    Guía de precio de carros usados
-                    </label>
+                <div class="pt-5 flex flex-col justify-center rounded-xl">
+                   <img id="headImg" src="https://64.media.tumblr.com/36967d731108d643d9b74bb3c5d2b5e1/747e52494a39b5da-e4/s1280x1920/b9b00fbfeb83f513fe3d09634c9dda906d7b3198.gifv"/>
+    
                 </div>
-                <div class="pt-12">
+                <div class="pt-12 justify-center">
                     <label class="text-xl font-semibold">
-                        Ingrese información del vehículo:
+                    
                     </label>
                 </div>
             </div>
@@ -19,11 +18,11 @@
                 <div class="lg:w-112 md:w-full">
                     <form
                       @submit.prevent="estimateCarPrice"
-                      class="px-8 py-5 space-y-2 rounded-xl bg-gray-100"
+                      class="px-8 py-5 space-y-2 rounded-xl bg-containers"
                     >
                       <!-- Brand field -->
                       <div>
-                        <label class="text-m font-bold text-gray-600 block"> Marca: </label>
+                        <label class="text-xl font-bold text-text block"> Marca </label>
                         <input
                           type="text"
                           name="brand"
@@ -33,7 +32,7 @@
                       </div>
                       <!-- Password field -->
                       <div class="">
-                        <label class="text-m font-bold text-gray-600 block"> Modelo: </label>
+                        <label class="text-xl font-bold text-text block"> Modelo </label>
                         <input
                           type="text"
                           name="model"
@@ -43,7 +42,7 @@
                       </div>
                       <!-- Year field -->
                       <div class="">
-                        <label class="text-m font-bold text-gray-600 block"> Año: </label>
+                        <label class="text-xl font-bold text-text block"> Año </label>
                         <input
                           type="number"
                           name="brand"
@@ -53,7 +52,7 @@
                       </div>
                       <!-- Kilometers field -->
                       <div class="">
-                        <label class="text-m font-bold text-gray-600 block"> Kilometraje: </label>
+                        <label class="text-xl font-bold text-text block"> Kilometraje </label>
                         <input
                           type="number"
                           name="kilometers"
@@ -63,7 +62,7 @@
                       </div>
                       <!-- Motor field -->
                       <div class="">
-                        <label class="text-m font-bold text-gray-600 block"> Motor: </label>
+                        <label class="text-xl font-bold text-text block"> Motor </label>
                         <input
                           type="text"
                           name="motor"
@@ -73,7 +72,7 @@
                       </div>
                       <!-- Transmission field -->
                       <div class="">
-                        <label class="text-m font-bold text-gray-600 block"> Transmisión: </label>
+                        <label class="text-xl font-bold text-text block"> Transmisión </label>
                         <select
                           name="transmission"
                           v-model="transmission"
@@ -86,24 +85,27 @@
                       <!-- Submit button -->
                       <div class="pt-3 flex justify-center">
                         <button
-                          class="py-2 px-4 bg-blue-500 hover:bg-blue-600 rounded-2xl text-white flex justify-center"
+                          class="py-2 px-4 bg-buttonF hover:bg-buttonB  text-text flex justify-center font-semibold text-m"
                         >
-                          Predecir precio
+                          PREDECIR PRECIO
                         </button>
                       </div>
                     </form>
-                    <div class="lg:w-112 border bg-gray-100 rounded-xl mt-5 px-8 py-5 font-semibold md:w-full text-lg">
-                        El precio estimado para su carro es: &nbsp; <label class="text-green-500 text-xl"> {{ price }} </label>
+                    <div class="lg:w-112 border bg-containers rounded-xl mt-5 px-8 py-5 font-semibold md:w-full text-lg">
+                      <label class="text-xl font-semibold text-text">
+                      El precio estimado para su carro es
+                      </label>
+                       &nbsp; <label class="text-price text-xl"> {{ price }} </label>
                     </div>
                 </div>        
                 <!-- rotating car gif -->
                 <div class="w-full flex flex-col justify-center lg:ml-10">
-                    <img @click="changeCarImg" id="carImg" src="https://drive.google.com/uc?export=view&id=1Qaoqn1xozn2kNRSFYsKiSNtdezcN-L_-" />
+                    <img @click="changeCarImg" id="carImg" src="https://drive.google.com/uc?export=view&id=1y0PZkQ5PNdlax_51j-VbvW_re7a11cb_" />
                 </div>
             </div>
             <!-- Footer -->
             <div id="footer" class="w-full bg-gray-100 h-16 mt-16">
-                <div class="p-5 text-gray-500"> © 2021 ICESI University </div>
+                <div class="p-5 text-text"> © 2021 ICESI University </div>
             </div>
         </main>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+        backgroundColor: '#1E1C40',
+        buttonF: '#AC47AA',
+        buttonB: '#AD71AC',
+        containers: '#3C396D',
+        
+    },
+    textColor:{
+      text: '#F1F1F1',
+      price: '#AC47AA',
+    },
     extend: {
       spacing: {
         "102": "28rem",
